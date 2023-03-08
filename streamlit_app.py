6843c77aeb31d3cf87558fa77d38212f16ac4931
reyesperez403100 <127309194+reyesperez403100@users.noreply.github.com>
2023-03-08 13:10:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('Menú de desayuno')
 streamlit.text('☕Omega 3 y avena con arándanos')
 streamlit.text('🥕Batido de col rizada, espinacas y rúcula')
 streamlit.text('🍉Huevo de gallinas camperas hervidas')
+
+streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
