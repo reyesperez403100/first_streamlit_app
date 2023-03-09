6301f8c28713a8e82711a1a2c905bb1ab53f64c9
reyesperez403100 <127309194+reyesperez403100@users.noreply.github.com>
2023-03-09 16:36:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit
 import pandas
 import snowflake.connector
+from urllib.error import URLError
 
 mi_lista_de_frutas = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 mi_lista_de_frutas = mi_lista_de_frutas.set_index('Fruit')
@@ -33,7 +34,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output as a table
 streamlit.dataframe(fruityvice_normalized)
-
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
